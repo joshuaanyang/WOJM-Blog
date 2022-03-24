7778140cc03cf93e432e73a828c79b845f3f416b
joshuaanyang <anyangjoshua@gmail.com>
2022-03-24 14:17:45 +0000

    changing database from sqlite to postgresql

--- main.py
+++ main.py
@@ -285,4 +285,6 @@ def delete_post(post_id):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0', port=5000)
